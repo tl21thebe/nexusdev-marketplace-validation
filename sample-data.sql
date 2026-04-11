8d74402059e4c5c0968b02fcad1f5593c56168d0
GiftMHB <giftmohuba159@gmail.com>
2026-04-11 20:00:14 +0200

    sample data with 10 books

---
+++ sample-data.sql
@@ -0,0 +1,134 @@
+INSERT INTO textbooks (title,isbn,edition,condition,annotation_level,extras,price,module_code
+) VALUES
+
+(
+    'Interaction Design',
+    '9781119901112',
+    4,
+    'Acceptable',
+    'Light',
+    '{
+        "access_code": "GFDZ",
+        "notes": "true"
+    }',
+    759.00,
+    'IMY310'
+),
+(
+    'Software Engineering',
+    '9781260792683',
+    2,
+    'Like New',
+    'None',
+    '{
+       "access_code": "7427",
+        "notes": "false" 
+    }'
+    1400.00,
+    'COS301'
+),
+(
+    'Concepts of Programming Languages',
+    '785284201153455',
+    12,
+    'Good',
+    'Heavy',
+    '{
+       "access_code": "FXVBV",
+        "notes": "false" 
+    }'
+    1090.00,
+    'COS333'
+),
+(
+    'Artificial Intelligence: A Modern Approach',
+    '9780136042594',
+    3,
+    'Good',
+    'None',
+    '{
+        "access_code": "414961",
+        "notes": "true"
+    }',
+    900.00,
+    'COS314'
+),
+(
+    'Mathematical Modelling',
+    '8742632126953',
+    5,
+    'Poor',
+    'Heavy',
+    '{
+        "access_code": "FFGH",
+        "notes": "false"
+    }'
+    700.00,
+    'WTW152'
+),
+(
+    'Software Modelling',
+    '89782600513878',
+    9,
+    'Poor',
+    'None',
+    '{
+        "access_code": "7891",
+        "notes": "false"
+    }'
+    250.00,
+    'COS214'
+),
+(
+    'District Mathematics',
+    '9780078223323',
+    8,
+    'Acceptable',
+    'Light',
+    '{
+        "access_code": "SXGDRV",
+        "notes": "true"
+    }'
+    650.00,
+    'WTW285'
+),
+(
+    'Imperative Programming',
+    '9876443246775',
+    5,
+    'Like New',
+    'Heavy',
+    '{
+        "access_code": "785625",
+        "notes": "true"
+    }'
+    999.99,
+    'COS132'
+),
+(
+    'Into to Computer',
+    '987689135568',
+    9,
+    'Poor',
+    'Light',
+    '{
+        "access_code": "MNBVC",
+        "notes": "false"
+    }'
+    200.00,
+    'COS151'
+),
+(
+    'Development',
+    '755632162566',
+    1,
+    'Like New',
+    'Light',
+    '{
+        "access_code": "ASDFG",
+        "notes": "false"
+    }'
+    2000.00,
+    'COS111'
+);
+
